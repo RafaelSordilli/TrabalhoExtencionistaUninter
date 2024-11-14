f05994599ed6fb50e41007e534a2554cf301214b
Rafael Sordilli <157806047+RafaelSordilli@users.noreply.github.com>
2024-11-14 12:44:34 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,15 @@
+function verificarResposta() {
+    const respostaCorreta = "2";
+    const respostaSelecionada = document.querySelector('input[name="answer"]:checked');
+    const resultadoElement = document.getElementById("resultado");
+  
+    resultadoElement.classList.remove("correta", "incorreta");
+
+    if (respostaSelecionada && respostaSelecionada.value === respostaCorreta) {
+        resultadoElement.textContent = "Correto! Você aprendeu a ligar o dispositivo.";
+        resultadoElement.classList.add("correta"); 
+    } else {
+        resultadoElement.textContent = "Tente novamente. Lembre-se de pressionar e segurar o botão de energia.";
+        resultadoElement.classList.add("incorreta"); 
+    }
+}
